b7d435fe6fc1072d4b52d67879c19041b9ae28c3
Timothy Pratley <timothypratley@gmail.com>
2017-08-03 21:01:26 -0700

    cleaning up some naming and produce the refer statement

--- src/cban/core.clj
+++ src/cban/core.clj
@@ -7,29 +7,69 @@
     [clojure.java.io :as io]))
 
 (def special-name
-  #{"&" "fn" "catch" "try" "finally"})
+  #{"fn" "catch" "try" "finally"})
 
-(defn defs [ns-to-translate rows]
-  (for [{:keys [existing alias docstring]} rows
+;; TODO: looking up meta requires the code as a dependency to be loaded... so can't run as a cmd line
+(defn defs [source-ns translations]
+  (for [{:keys [existing alias docstring]} translations
         :when (and existing alias)
-        :let [{:keys [special-form macro]} (meta (ns-resolve (symbol ns-to-translate) (symbol existing)))]]
+        :let [{:keys [special-form macro]} (meta (ns-resolve (symbol source-ns) (symbol existing)))]]
     (if (or (special-name existing) special-form macro)
-      (str "(defmacro " alias " [& body]\n  `(" existing " ~@body))")
-      (str "(def " alias " " existing ")"))))
+      (str "(defmacro " alias "\n"
+           (when docstring (str "  \"" docstring "\"\n"))
+           "  [& body]\n  `(z/" existing " ~@body))")
+      (str "(def " alias "\n"
+           (when docstring (str "  \"" docstring "\"\n"))
+           "  z/" existing ")"))))
 
-(defn generate [file ns-to-translate translations]
-  (str "(ns cban." file ")\n\n;; This file was generated, do not modify it directly\n\n"
+(defn generate-ns [source-ns destination-ns translations]
+  (str "(ns cban." destination-ns
+       "\n  (:require [" source-ns " :as z])\n\n;; This file was generated, do not modify it directly\n\n"
        (clojure.string/join "\n\n"
-         (defs ns-to-translate translations))
+         (defs source-ns translations))
        "\n"))
 
-(defn -main [& args]
+(defn macro? [source-ns x]
+  (or (special-name x)
+      (let [{:keys [special-form macro]} (meta (ns-resolve (symbol source-ns) (symbol x)))]
+        (or special-form macro))))
+
+(defn generate-refer [source-ns destination-ns translations]
+  (let [alias (for [{:keys [existing alias]} translations
+                    :when (and existing alias)]
+                alias)]
+    (str "(require '[" destination-ns
+         " :refer [" (string/join " " (remove #(macro? source-ns %) alias)) "]"
+         " :refer-macros [" (string/join " " (filter #(macro? source-ns %) alias)) "]])")))
+
+(defn generate-translations []
   (doseq [[root dirs files] (fs/iterate-dir "translations")
           :when (seq files)
-          :let [ns-to-translate (fs/base-name root)]
+          :let [source-ns (fs/base-name root)]
           file files
-          :let [translations (c/slurp-csv (io/file root file))]]
-    (println "Writing" ns-to-translate files)
-    (spit (io/file "out" ns-to-translate
-                   (str file ".cljc"))
-          (generate file ns-to-translate translations))))
+          :let [translations (c/slurp-csv (io/file root file))
+                destination-ns (str (string/replace source-ns "." "-")
+                                    "-"
+                                    (fs/base-name file ".csv"))]]
+    (println "Writing" source-ns files)
+    (spit (io/file "out" "cban" (str destination-ns ".cljc"))
+          (generate-ns source-ns destination-ns translations))))
+
+(defn generate-refers []
+  (doseq [[root dirs files] (fs/iterate-dir "translations")
+          :when (seq files)
+          :let [source-ns (fs/base-name root)]
+          file files
+          :let [translations (c/slurp-csv (io/file root file))
+                destination-ns (str (string/replace source-ns "." "-")
+                                    "-"
+                                    (fs/base-name file ".csv"))]]
+    (println "Writing" source-ns files)
+    (spit (io/file "out" "cban" (str destination-ns ".cljc"))
+          (generate-ns source-ns destination-ns translations))
+    (spit (io/file "out" "cban" (str destination-ns ".refer"))
+          (generate-refer source-ns destination-ns translations))))
+
+(defn -main [& args]
+  (generate-translations)
+  (generate-refers))
