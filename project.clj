234e6808350e267eaf2251bb785cd52aa577f223
Timothy Pratley <timothypratley@gmail.com>
2017-08-13 14:40:58 -0700

    name the map clojure-core

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cban "0.1.0-SNAPSHOT"]]
   :cban {:input-dir "translations"
-         :output-dir "src/cban"})
+         :output-dir "src"
+         :output-map-to "resources/clojure-core-translations-map.edn"})
