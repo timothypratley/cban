ebd6f1de641e1dac26f56ae355181770e9da6d4a
Timothy Pratley <timothypratley@gmail.com>
2017-10-10 15:08:05 -0400

    preparing for release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject cban "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Translations for clojure.core"
+  :url "http://github.com/timothypratley/cban"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cban "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cban "0.1.0"]]
   :cban {:input-dir "translations"
          :output-dir "src"
          :output-map-to "resources/clojure-core-translations-map.edn"})
